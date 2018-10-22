b296282f80ac4e2cd57b8e30184ad4f9f88ebe30
Chris Lee <sihrc.c.lee@gmail.com>
2018-10-22 14:00:35 -0400

    FIX: organizations tests

--- tests/text/test_organizations.py
+++ tests/text/test_organizations.py
@@ -6,14 +6,14 @@ from .indico_text_base import TextTest
 class OrganizationsTest(TextTest):
 
     def test_organizations_v2(self):
-        test_data = "A year ago, the New York Times published confidential comments about ISIS' ideology by Major General Michael K. Nagata, then U.S. Special Operations commander in the Middle East."
+        test_data = "A year ago, the New York Times published confidential comments about ISIS ideology by Major General Michael K. Nagata, then U.S. Special Operations commander in the Middle East."
         response = organizations(test_data)
         self.assertTrue(isinstance(response, list))
         sorted_response = sorted(response, key=lambda x: x['confidence'], reverse=True)
-        self.assertTrue('ISIS' in [result["text"] for result in sorted_response])
+        self.assertTrue('New York Times' in [result["text"] for result in sorted_response])
 
         test_data = [test_data] * 2
         response = organizations(test_data)
         self.assertTrue(isinstance(response, list))
         sorted_response = [sorted(arr, key=lambda x: x['confidence'], reverse=True) for arr in response]
-        self.assertTrue('ISIS' in [result["text"] for result in sorted_response[0]])
+        self.assertTrue('New York Times' in [result["text"] for result in sorted_response[0]])
