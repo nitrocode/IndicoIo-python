5b5920884d3fe1cc5b4efdc8b4040f353c2a89e0
rbarghou <ramsey@indico.io>
2019-08-21 15:31:35 -0400

    updated to put requirements back in setup.py

--- Py3.Dockerfile
+++ Py3.Dockerfile
@@ -6,10 +6,7 @@ ENV INDICO_API_HOST="apiv2.indico.io"
 RUN apk add --no-cache libjpeg jpeg-dev zlib-dev && \
     pip3 install "mock>=1.3.0<2.0.0" "nose>=1.0" setuptools
 
-COPY requirements.py requirements.py
 COPY README.rst README.rst
-
-RUN pip3 install $(python3 -c "from requirements import REQUIREMENTS; print(' '.join(REQUIREMENTS))")
 
 COPY . /indicoio-python
 WORKDIR /indicoio-python
--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 """
 Setup for indico apis
 """
-from requirements import REQUIREMENTS
+import sys
+
+PY2 = True if sys.version_info[0] == 2 else False
+PY3 = not PY2
+
+REQUIREMENTS = [
+    "msgpack==0.5.6",
+    "msgpack-numpy==0.4.1",
+    "Pillow<6.0.0",
+    "requests>=2.2.1",
+    "setuptools==36.5.0",
+    "six>=1.3.0",
+]
+
+# shim for futures support
+if PY2:
+    REQUIREMENTS.append("futures>=3.0.0")
+    REQUIREMENTS.append("numpy<1.17")
+else:
+    REQUIREMENTS.append("numpy>=1.14.1")
 
 try:
     from setuptools import setup
